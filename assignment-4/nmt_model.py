7d7bd8da8af4274084a68cec90d941dfdc1d2484
yottacto <yotta.w.cto@gmail.com>
2019-05-19 14:06:44 +0800

    Working on nmt

--- assignment-4/nmt_model.py
+++ assignment-4/nmt_model.py
@@ -277,6 +277,16 @@ class NMT(nn.Module):
         ###     Tensor Stacking:
         ###         https://pytorch.org/docs/stable/torch.html#torch.stack
 
+        enc_hiddens_proj = self.att_projection(enc_hiddens)
+        indices = torch.tensor(self.vocab.src.words2indices(target_padded.tolist()))
+        Y = self.model_embeddings.target(indices)
+        for t in torch.split(Y, 1):
+            Y_t = torch.squeeze(t, 0)
+            Ybar_t = torch.cat((Y_t, o_prev), dim=1)
+            dec_state, o_t, e_t = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            combined_outputs.append(o_t)
+            o_prev = o_t
+        combined_outputs = torch.stack(combined_outputs)
 
         ### END YOUR CODE
 
